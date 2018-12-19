0279fae93cb3df53f86ca820df321094c8e4ef43
daniel cruz <dannyfsociety@gmail.com>
2018-12-18 23:42:03 -0400

    Some validations were added :zap: :warning:

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,10 +1,11 @@
+import { FileItem } from './../models/file-item';
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
-
+  @Input() archives: FileItem[] = [];
   @Output() mouseOver: EventEmitter<boolean> = new EventEmitter();
   
   constructor() { }
@@ -14,8 +15,31 @@ export class NgDropFilesDirective {
     this.mouseOver.emit(true);
   }
 
-  @HostListener('dragLeave', ['$event'])
+  @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any){
     this.mouseOver.emit(false);
+  }
+  /**validations */
+  private canArchiveBeDropped(archive: File) {
+    if (!this.archiveAlreadyDropped(archive.name) && this.isImage(archive.type)) {
+      return true;  
+    } else {
+      return false;
+    }
+  }
+  private preventAndStop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  private archiveAlreadyDropped(archiveName: string): boolean {
+    for (const archive of this.archives) {
+      if (archive.archiveName === archiveName) {
+        return true;
+      }
+    }
+    return false;
+  }
+  private isImage(archiveType: string) {
+    return (archiveType === '' || archiveType === undefined) ? false : archiveType.startsWith('image'); 
   }
 }
